593af2728ed694ae7fe15713aa50e3b3564213c4
Longfei Jiao <ljiao@ualberta.ca>
2025-03-22 11:16:11 -0600

    update compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.simplehealthmanagement"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.simplehealthmanagement"
