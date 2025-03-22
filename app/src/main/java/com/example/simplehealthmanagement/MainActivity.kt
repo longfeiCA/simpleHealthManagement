6d0d7535c4899f4ae835c9933a54d3eb0e929f29
Longfei Jiao <longfeijiao1230@gmail.com>
2025-03-22 13:25:52 -0600

    MainActivity review complete

--- app/src/main/java/com/example/simplehealthmanagement/MainActivity.kt
+++ app/src/main/java/com/example/simplehealthmanagement/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.viewmodel.compose.viewModel
+/*  Compare activities:
+ComponentActivity:
+    Purpose: A more modern and leaner base class for activities, introduced as part of Android Jetpack's Activity library. 
+    It focuses on providing core activity functionality and integration with other Jetpack components.
 
+What we used in Lucky-Omelet: AppCompatActivity
+
+    Purpose: Primarily designed to provide backward compatibility for newer Android features on older devices. 
+    It allows you to use features like the Action Bar (now Toolbar), Material Design themes, 
+    and fragment support even on devices running older versions of Android.
+    Traditionally, AppCompatActivity was the go-to base class for most Android applications using the traditional Android UI toolkit (Views).
+*/
 class MainActivity : ComponentActivity() {
+    // variableName: Type for savedInstanceState: Bundle?
+    // ? means it may be null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel: HealthViewModel = viewModel()
-            HealthScreen(viewModel)
+            val viewModel: HealthViewModel = viewModel() // parameter required for a Composable function 
+            HealthScreen(viewModel) // Composable function
         }
     }
 }
