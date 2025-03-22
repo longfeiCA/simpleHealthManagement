150b82b21743fdc176c772a1efa75a461205452a
Longfei Jiao <longfeijiao1230@gmail.com>
2025-03-22 13:12:30 -0600

    ViewModel review complete

--- app/src/main/java/com/example/simplehealthmanagement/HealthViewModel.kt
+++ app/src/main/java/com/example/simplehealthmanagement/HealthViewModel.kt
@@ -9,12 +9,14 @@ import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class HealthViewModel : ViewModel() {
+    // private val should start with "_"
     private val _temperature = MutableStateFlow(37.0) // 体温
     private val _heartRate = MutableStateFlow(70) // 心率
     private val _bloodPressure = MutableStateFlow(115) // 血压
     private val _spo2 = MutableStateFlow(95) // 血氧
     private val _isAlert = MutableStateFlow(false) // 是否报警
 
+    // Similar to getters in Java. Expose these values to UI safely (UI cannot modify them)
     val temperature: StateFlow<Double> = _temperature
     val heartRate: StateFlow<Int> = _heartRate
     val bloodPressure: StateFlow<Int> = _bloodPressure
@@ -26,10 +28,13 @@ class HealthViewModel : ViewModel() {
     }
 
     private fun startUpdatingData() {
+        /*  Start a coroutine bounded to the lifecycle of ViewModel.
+        This avoid unnecessary resource consumption after the user navigates away form the screen 
+        and the associated Activity or Fragmnent is destroyed.  */
         viewModelScope.launch {
             while (true) {
-                _temperature.value = Random.nextDouble(35.5, 39.5)
-                _heartRate.value = Random.nextInt(50, 200)
+                _temperature.value = Random.nextDouble(35.5, 38.5)
+                _heartRate.value = Random.nextInt(50, 120)
                 _bloodPressure.value = Random.nextInt(90, 140)
                 _spo2.value = Random.nextInt(85, 100)
 
